3e5ddca72efb3e1a7406c5da008a392d55442acd
Muhammad Mostey <muhammadmostey@gmail.com>
2024-08-08 13:03:38 +0300

    creates basic table for Pantry items

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { useStyle } from "@mui/material/styles";
 import homeStyles from "./style";
 export default function Home() {
   const classes = homeStyles;
+  let items = ["potato", "tomato", "tomato", "tomato", "tomato", "tomato"];
   return (
     <Box sx={classes.main}>
-      <Box>Pantry Items</Box>
+      <Box sx={classes.boxTable}>
+        <Box sx={classes.boxHeader}>
+          <Typography sx={classes.headerText} variant={"h2"}>
+            Pantry Items
+          </Typography>
+        </Box>
+        <Stack sx={classes.boxBody} spacing={2}>
+          {items.map((i) => (
+            <Box sx={classes.itemBox} key={i}>
+              <Typography sx={classes.itemText} variant={"h3"}>
+                {i.charAt(0).toUpperCase() + i.slice(1)}
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
