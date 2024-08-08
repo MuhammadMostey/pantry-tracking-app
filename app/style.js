17c50844d2e3929b07fd64958008a423b94b8cdf
Muhammad Mostey <muhammadmostey@gmail.com>
2024-08-08 13:03:15 +0300

    adds basic styles to the app

---
+++ app/style.js
@@ -0,0 +1,48 @@
+const homeStyles = {
+  main: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  boxTable: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px solid #333",
+  },
+  boxHeader: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ADD8E6",
+  },
+  headerText: {
+    color: "#333",
+    textAlign: "center",
+  },
+  boxBody: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "auto",
+    height: "300px",
+    width: "800px",
+  },
+  itemBox: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "300px",
+    width: "100%",
+    backgroundColor: "#f0f0f0",
+  },
+  itemText: {
+    color: "#333",
+    textAlign: "center",
+  },
+};
+
+export default homeStyles;
