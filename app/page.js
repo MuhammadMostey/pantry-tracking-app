2e8021762453131cb61736b52ff2f4b4b130e09e
Muhammad Mostey <muhammadmostey@gmail.com>
2024-08-12 19:18:19 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 import { Box, Stack, Typography } from "@mui/material";
 import { useStyle } from "@mui/material/styles";
 import homeStyles from "./style";
+import db from "../firebase";
+import { getDoc, collection } from "firebase/firestore";
+
 export default function Home() {
   const classes = homeStyles;
   let items = ["potato", "tomato", "tomato", "tomato", "tomato", "tomato"];
+
+  // geting data from the db
+  const data = async () => {
+    const d = await getDoc(collection(db, "pantry"));
+    console.log(d);
+  };
+
   return (
     <Box sx={classes.main}>
       <Box sx={classes.boxTable}>
