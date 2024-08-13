6962f33e91619752353bdfa391b573419429c7e8
Muhammad Mostey <muhammadmostey@gmail.com>
2024-08-13 20:48:29 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,53 @@
+"use client";
+// react
+import { useEffect, useState } from "react";
+
+// mui
 import { Box, Stack, Typography } from "@mui/material";
 import { useStyle } from "@mui/material/styles";
 import homeStyles from "./style";
-import db from "../firebase";
-import { getDoc, collection } from "firebase/firestore";
+
+// firebase
+import { db } from "../firebase";
+import {
+  getDoc,
+  getDocs,
+  collection,
+  firestore,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
+
+
+let items = ["potato", "tomato", "tomato", "tomato", "tomato", "tomato"];
 
 export default function Home() {
   const classes = homeStyles;
-  let items = ["potato", "tomato", "tomato", "tomato", "tomato", "tomato"];
 
   // geting data from the db
-  const data = async () => {
-    const d = await getDoc(collection(db, "pantry"));
-    console.log(d);
-  };
+  // const data = async () => {
+  //   const d = await getDoc(collection(db, "pantry"));
+  //   console.log(d);
+  // };
+
+  const [pantryItems, setPantryItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const updatePantry = () => {
+      const q = query(collection(db, "pantry"));
+      const unsubscribe = onSnapshot(q, (docs) => {
+        const pantryList = [];
+        docs.forEach((doc) => {
+          pantryList.push(doc.id);
+        });
+        setPantryItems(pantryList);
+        setLoading(false);
+      });
+      return unsubscribe;
+    };
+    updatePantry();
+  }, []);
 
   return (
     <Box sx={classes.main}>
@@ -23,13 +58,17 @@ export default function Home() {
           </Typography>
         </Box>
         <Stack sx={classes.boxBody} spacing={2}>
-          {items.map((i) => (
-            <Box sx={classes.itemBox} key={i}>
-              <Typography sx={classes.itemText} variant={"h3"}>
-                {i.charAt(0).toUpperCase() + i.slice(1)}
-              </Typography>
-            </Box>
-          ))}
+          {loading ? (
+            <Box sx={classes.loading}>loading..</Box>
+          ) : (
+            pantryItems.map((i) => (
+              <Box sx={classes.itemBox} key={i}>
+                <Typography sx={classes.itemText} variant={"h3"}>
+                  {i.charAt(0).toUpperCase() + i.slice(1)}
+                </Typography>
+              </Box>
+            ))
+          )}
         </Stack>
       </Box>
     </Box>
